92d01db3c942d98cdd722d052444e5321f99df76
skins <malachy.byrne257@mail.dcu.ie>
2021-02-05 22:46:58 +0000

    added add product form

--- ca298_project/project_app/urls.py
+++ ca298_project/project_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('registration/', views.register, name="register"),
-    path('all_products', views.all_products, name="products"),
-    path('product/<int:prod_id>', views.singleproduct, name="product_single")
+    path('all_products/', views.all_products, name="products"),
+    path('product/<int:prod_id>', views.singleproduct, name="product_single"),
+    path('productform/', views.product_form, name="add_product")
 ]
--- ca298_project/project_app/views.py
+++ ca298_project/project_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from .forms import *
 from .models import *
 
 
@@ -13,9 +14,20 @@ def register(request):
 
 def all_products(request):
     all_p = Product.objects.all()
-    return render(request, 'all_vegetables.html', {"products": all_p})
+    return render(request, 'all_vegetables.html', {'products': all_p})
 
 
 def singleproduct(request, prod_id):
     prod = get_object_or_404(Product, pk=prod_id)
     return render(request, 'single_product.html', {'product': prod})
+
+
+def product_form(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            prod = form.save()
+            return render(request, 'single_product.html', {'product': prod})
+    else:
+        form = ProductForm()
+        return render(request, 'form.html', {'form': form})
