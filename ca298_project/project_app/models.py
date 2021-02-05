db9e7a13a52ecd15d3207ca7edb0061de1986a2b
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-02-05 15:41:00 +0000

    Create product and order models

--- ca298_project/project_app/models.py
+++ ca298_project/project_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def price(self):
+        return self.product.price * self.quantity
