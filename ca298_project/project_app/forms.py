d89bac794d21144043ea0b21ed9c5ac46bca4bff
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-02-25 23:37:31 +0000

    Create admin signup form and user login form

--- ca298_project/project_app/forms.py
+++ ca298_project/project_app/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
-from .models import *
+from .models import Product, User
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class ProductForm(ModelForm):
@@ -20,3 +22,23 @@ class SignupForm(UserCreationForm):
         user.is_admin = False
         user.save()
         return user
+
+
+class AdminSignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'})
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '', 'id': 'hi'}))
