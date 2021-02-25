079ea58bccbe5c7ce3091dc5a1dd62e9fe79d069
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-02-25 16:27:24 +0000

    Create user registration form

--- ca298_project/project_app/forms.py
+++ ca298_project/project_app/forms.py
@@ -1,8 +1,22 @@
 from django.forms import ModelForm
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price']
+
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_admin = False
+        user.save()
+        return user
