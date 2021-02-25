0f9061084f9957551c52eb3d95cd7d6efe11d23a
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-02-25 16:27:34 +0000

    Create user registration view

--- ca298_project/project_app/views.py
+++ ca298_project/project_app/views.py
@@ -2,6 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .forms import *
 from .models import *
+from django.views.generic import CreateView
+from django.contrib.auth import login
+
+
+class UserSignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'user_signup.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
 
 
 def index(request):
