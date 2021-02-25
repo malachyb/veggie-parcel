a4e4a8048990247dcf45138d08dc5e4833fb362e
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-02-25 23:35:55 +0000

    Setup urls for adminsignup, login and logout

--- ca298_project/project_app/urls.py
+++ ca298_project/project_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .forms import UserLoginForm
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -7,5 +8,8 @@ urlpatterns = [
     path('all_products/', views.all_products, name="products"),
     path('product/<int:prod_id>', views.singleproduct, name="product_single"),
     path('productform/', views.product_form, name="add_product"),
-    path('usersignup/', views.UserSignupView.as_view(), name="register")
+    path('usersignup/', views.UserSignupView.as_view(), name="register"),
+    path('adminsignup/', views.AdminSignupView.as_view(), name="Admin register"),
+    path('login/', views.Login.as_view(template_name="login.html", authentication_form=UserLoginForm), name='login'),
+    path('logout/', views.logout_view, name="logout")
 ]
