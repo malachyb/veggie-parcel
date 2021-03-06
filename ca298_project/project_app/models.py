c219e392934242624019dfe11d1d12d722006eca
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-03-06 00:19:35 +0000

    Create new ProductCategory model, added picture & category to Product model

--- ca298_project/project_app/models.py
+++ ca298_project/project_app/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class ProductCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    picture = models.FileField(upload_to='product_images/', blank=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True)
 
 
 class Order(models.Model):
