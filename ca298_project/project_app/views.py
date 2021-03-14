857090f06bd65ab30d5334af877af603e1c51f43
skins <malachy.byrne257@mail.dcu.ie>
2021-03-14 22:15:42 +0000

    show quantity on single order view

--- ca298_project/project_app/views.py
+++ ca298_project/project_app/views.py
@@ -135,5 +135,9 @@ def all_orders(request):
 @admin_required
 def view_order(request, order_id):
     order_products = OrderItems.objects.filter(order_id=order_id)
-    products = [Product.objects.get(id=o.product_id) for o in order_products]
+    products = []
+    for o in order_products:
+        p = Product.objects.get(id=o.product_id)
+        p.quantity = o.quantity
+        products.append(p)
     return render(request, "single_order.html", {"products": products})
