b6eb0a5a887899c956f2644765098e2568263b0a
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-03-18 10:50:37 +0000

    Register models in admin.py

--- ca298_project/project_app/admin.py
+++ ca298_project/project_app/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Vegetable)
 admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(OrderItems)
+admin.site.register(Product)
 admin.site.register(ProductCategory)
+admin.site.register(Basket)
+admin.site.register(BasketItems)
+admin.site.register(Vegetable)
