b7ae56349f97b72a579ad56b23e05f8c6425088c
Qiyang Zuo <qiyang.zuo2@mail.dcu.ie>
2021-04-01 22:17:02 +0100

    Created serializers.py & added UserSerializer and ProductSerializer

---
+++ ca298_project/project_app/serializers.py
@@ -0,0 +1,14 @@
+from .models import User, Product
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
